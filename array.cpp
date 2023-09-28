eb59e782ce2ffc9ab99ef86e879fff3d7ec1c9d6
umartils <146210433+umartils@users.noreply.github.com>
2023-09-28 22:13:30 +0700

    Update array.cpp
    Jadi full pakai array

--- array.cpp
+++ array.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-struct buah {
-    string nama;
-    string warna;
-    float berat;
-    int harga;
-    string rasa;
-};
-
-const int MAX_SIZE = 100; // Define the maximum size of the array
-buah listBuah[MAX_SIZE];   // Array to store buah elements
-int currentSize = 0;       // Current number of elements in the array
+const int MAX_SIZE = 100; // Batas maksimum jumlah elemen
+string nama[MAX_SIZE];
+string warna[MAX_SIZE];
+float berat[MAX_SIZE];
+int harga[MAX_SIZE];
+string rasa[MAX_SIZE];
+int currentSize = 0; // Jumlah elemen saat ini
 
 /*
- * Prototypes of functions
+ * Prototipe Fungsi
  */
-void buatSingleArray(string, string, float, int, string);
+void buatElemen(string, string, float, int, string);
 void masukanElemenKeAwal(string, string, float, int, string);
 void masukanElemenKeAkhir(string, string, float, int, string);
 void rubahDataElemenPertama(string, string, float, int, string);
@@ -26,81 +21,122 @@ void hapusElemenPertama();
 void hapusElemenTerakhir();
 void cetakSingleArray();
 void cetakSeluruhAtributElemen();
-void cetakArray();
 
 /*
- * Main function
+ * Fungsi Utama
  */
 int main() {
-    cetakArray();
+    /* Membuat elemen pertama */
+    cout << endl << "01. Membuat Elemen Pertama:" << endl;
+    buatElemen("Mangga", "Jingga", 1.1, 10000, "Manis");
+    cetakSingleArray(); // Menampilkan elemen
+
+    /* Menambahkan elemen di awal */
+    cout << endl << "02. Menambahkan elemen di awal:" << endl;
+    masukanElemenKeAwal("Pisang", "Kuning", 1.3, 8000, "Manis");
+    cetakSingleArray(); // Menampilkan elemen
+
+    /* Menambahkan elemen di akhir */
+    cout << endl << "03. Menambahkan elemen di akhir:" << endl;
+    masukanElemenKeAkhir("Apel", "Merah", 2.1, 13000, "Manis");
+    cetakSingleArray(); // Menampilkan elemen
+
+    /* Menambahkan elemen di awal lagi */
+    cout << endl << "04. Menambahkan elemen di awal lagi:" << endl;
+    masukanElemenKeAwal("Anggur", "Ungu", 1, 9000, "Manis");
+    cetakSingleArray(); // Menampilkan elemen
+
+    /* Menghapus elemen di awal */
+    cout << endl << "05. Menghapus elemen di awal:" << endl;
+    hapusElemenPertama();
+    cetakSingleArray();
+
+    /* Menghapus elemen di akhir */
+    cout << endl << "06. Menghapus elemen di akhir:" << endl;
+    hapusElemenTerakhir();
+    cetakSingleArray();
+
+    /* Menghapus elemen di akhir lagi */
+    cout << endl << "07. Menghapus elemen di akhir lagi:" << endl;
+    hapusElemenTerakhir();
+    cetakSingleArray();
 
     return 0;
-} // end of main
+} // Akhir dari main
 
 /*
- * Functions
+ * Fungsi
  */
 
-void buatSingleArray(string nama, string warna, float berat, int harga, string rasa) {
+void buatElemen(string n, string w, float b, int h, string r) {
     if (currentSize < MAX_SIZE) {
-        listBuah[currentSize].nama = nama;
-        listBuah[currentSize].warna = warna;
-        listBuah[currentSize].berat = berat;
-        listBuah[currentSize].harga = harga;
-        listBuah[currentSize].rasa = rasa;
+        nama[currentSize] = n;
+        warna[currentSize] = w;
+        berat[currentSize] = b;
+        harga[currentSize] = h;
+        rasa[currentSize] = r;
         currentSize++;
     }
 }
 
-void masukanElemenKeAwal(string nama, string warna, float berat, int harga, string rasa) {
+void masukanElemenKeAwal(string n, string w, float b, int h, string r) {
     if (currentSize < MAX_SIZE) {
         for (int i = currentSize; i > 0; i--) {
-            listBuah[i] = listBuah[i - 1];
+            nama[i] = nama[i - 1];
+            warna[i] = warna[i - 1];
+            berat[i] = berat[i - 1];
+            harga[i] = harga[i - 1];
+            rasa[i] = rasa[i - 1];
         }
-        listBuah[0].nama = nama;
-        listBuah[0].warna = warna;
-        listBuah[0].berat = berat;
-        listBuah[0].harga = harga;
-        listBuah[0].rasa = rasa;
+        nama[0] = n;
+        warna[0] = w;
+        berat[0] = b;
+        harga[0] = h;
+        rasa[0] = r;
         currentSize++;
     }
 }
 
-void masukanElemenKeAkhir(string nama, string warna, float berat, int harga, string rasa) {
+void masukanElemenKeAkhir(string n, string w, float b, int h, string r) {
     if (currentSize < MAX_SIZE) {
-        listBuah[currentSize].nama = nama;
-        listBuah[currentSize].warna = warna;
-        listBuah[currentSize].berat = berat;
-        listBuah[currentSize].harga = harga;
-        listBuah[currentSize].rasa = rasa;
+        nama[currentSize] = n;
+        warna[currentSize] = w;
+        berat[currentSize] = b;
+        harga[currentSize] = h;
+        rasa[currentSize] = r;
         currentSize++;
     }
 }
 
-void rubahDataElemenPertama(string nama, string warna, float berat, int harga, string rasa) {
+void rubahDataElemenPertama(string n, string w, float b, int h, string r) {
     if (currentSize > 0) {
-        listBuah[0].nama = nama;
-        listBuah[0].warna = warna;
-        listBuah[0].berat = berat;
-        listBuah[0].harga = harga;
-        listBuah[0].rasa = rasa;
+        nama[0] = n;
+        warna[0] = w;
+        berat[0] = b;
+        harga[0] = h;
+        rasa[0] = r;
     }
 }
 
-void rubahDataElemenTerakhir(string nama, string warna, float berat, int harga, string rasa) {
+void rubahDataElemenTerakhir(string n, string w, float b, int h, string r) {
     if (currentSize > 0) {
-        listBuah[currentSize - 1].nama = nama;
-        listBuah[currentSize - 1].warna = warna;
-        listBuah[currentSize - 1].berat = berat;
-        listBuah[currentSize - 1].harga = harga;
-        listBuah[currentSize - 1].rasa = rasa;
+        int lastIndex = currentSize - 1;
+        nama[lastIndex] = n;
+        warna[lastIndex] = w;
+        berat[lastIndex] = b;
+        harga[lastIndex] = h;
+        rasa[lastIndex] = r;
     }
 }
 
 void hapusElemenPertama() {
     if (currentSize > 0) {
         for (int i = 0; i < currentSize - 1; i++) {
-            listBuah[i] = listBuah[i + 1];
+            nama[i] = nama[i + 1];
+            warna[i] = warna[i + 1];
+            berat[i] = berat[i + 1];
+            harga[i] = harga[i + 1];
+            rasa[i] = rasa[i + 1];
         }
         currentSize--;
     }
@@ -114,48 +150,7 @@ void hapusElemenTerakhir() {
 
 void cetakSingleArray() {
     for (int i = 0; i < currentSize; i++) {
-        cout << listBuah[i].nama << " >> ";
+        cout << nama[i] << " >> ";
     }
     cout << "!!\n";
 }
-void cetakArray(){
-    cout <<"01. Creating the First Element:" << endl;
-    buatSingleArray("Mangga", "Jingga", 1.1, 10000, "Manis");
-    cetakSingleArray(); // Display the elements
-
-    /* Add an element at the beginning */
-    cout <<"02. Adding an Element at the Beginning:" << endl;
-    masukanElemenKeAwal("Pisang", "Kuning", 1.3, 8000, "Manis");
-    cetakSingleArray(); // Display the elements
-
-    /* Add an element at the end */
-    cout <<"03. Adding an Element at the End:" << endl;
-    masukanElemenKeAkhir("Apel", "Merah", 2.1, 13000, "Manis");
-    cetakSingleArray(); // Display the elements
-
-    /* Add an element at the beginning again */
-    cout <<"04. Adding an Element at the Beginning Again:" << endl;
-    masukanElemenKeAwal("Anggur", "Ungu", 1, 9000, "Manis");
-    cetakSingleArray(); // Display the elements
-
-    /* Remove the first element */
-    cout <<"05. Removing the First Element:" << endl;
-    hapusElemenPertama();
-    cetakSingleArray();
-
-    /* Add an element at the end */
-    cout <<"03. Adding an Element at the End:" << endl;
-    masukanElemenKeAkhir("Khuldi", "Merah", 2.1, 13000, "Manis");
-    cetakSingleArray(); // Display the elements
-
-    /* Remove the last element */
-    cout <<"06. Removing the Last Element:" << endl;
-    hapusElemenTerakhir();
-    cetakSingleArray();
-
-    /* Remove the last element again */
-    cout <<"07. Removing the Last Element Again:" << endl;
-    hapusElemenTerakhir();
-    cetakSingleArray();
-}
-/*satria*/
