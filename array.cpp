8d127c39f93b9cb6c3ed3b798c90fd6b3a94a73e
sSHAITON <146210590+sSHAITON@users.noreply.github.com>
2023-10-01 22:26:30 +0700

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
-using namespace std;
 
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+// deklarasi array
 const int MAX_SIZE = 100; // Batas maksimum jumlah elemen
 string nama[MAX_SIZE];
 string warna[MAX_SIZE];
@@ -9,6 +14,7 @@ int harga[MAX_SIZE];
 string rasa[MAX_SIZE];
 int currentSize = 0; // Jumlah elemen saat ini
 
+// Prototype fungsi array
 void buatElemen(string, string, float, int, string);
 void masukanElemenKeAwal(string, string, float, int, string);
 void masukanElemenKeAkhir(string, string, float, int, string);
@@ -18,48 +24,21 @@ void hapusElemenPertama();
 void hapusElemenTerakhir();
 void cetakSingleArray();
 void cetakSeluruhAtributElemen();
+void cetakArray();
+void waktuArray();
 
-int main() {
-    /* Membuat elemen pertama */
-    cout << endl << "01. Membuat Elemen Pertama:" << endl;
-    buatElemen("Mangga", "Jingga", 1.1, 10000, "Manis");
-    cetakSingleArray(); // Menampilkan elemen
-
-    /* Menambahkan elemen di awal */
-    cout << endl << "02. Menambahkan elemen di awal:" << endl;
-    masukanElemenKeAwal("Pisang", "Kuning", 1.3, 8000, "Manis");
-    cetakSingleArray(); // Menampilkan elemen
-
-    /* Menambahkan elemen di akhir */
-    cout << endl << "03. Menambahkan elemen di akhir:" << endl;
-    masukanElemenKeAkhir("Apel", "Merah", 2.1, 13000, "Manis");
-    cetakSingleArray(); // Menampilkan elemen
-
-    /* Menambahkan elemen di awal lagi */
-    cout << endl << "04. Menambahkan elemen di awal lagi:" << endl;
-    masukanElemenKeAwal("Anggur", "Ungu", 1, 9000, "Manis");
-    cetakSingleArray(); // Menampilkan elemen
-
-    /* Menghapus elemen di awal */
-    cout << endl << "05. Menghapus elemen di awal:" << endl;
-    hapusElemenPertama();
-    cetakSingleArray();
-
-    /* Menghapus elemen di akhir */
-    cout << endl << "06. Menghapus elemen di akhir:" << endl;
-    hapusElemenTerakhir();
-    cetakSingleArray();
-
-    /* Menghapus elemen di akhir lagi */
-    cout << endl << "07. Menghapus elemen di akhir lagi:" << endl;
-    hapusElemenTerakhir();
-    cetakSingleArray();
-
+int main()
+{
+    cout << "\nHasil Array\n";
+    cout << "====================================================";
+    waktuArray();
     return 0;
-} 
-
-void buatElemen(string a, string b, float c, int d, string e) {
-    if (currentSize < MAX_SIZE) {
+}
+// fungsi array
+void buatElemen(string a, string b, float c, int d, string e)
+{
+    if (currentSize < MAX_SIZE)
+    {
         nama[currentSize] = a;
         warna[currentSize] = b;
         berat[currentSize] = c;
@@ -69,9 +48,12 @@ void buatElemen(string a, string b, float c, int d, string e) {
     }
 }
 
-void masukanElemenKeAwal(string a, string b, float c, int d, string e) {
-    if (currentSize < MAX_SIZE) {
-        for (int i = currentSize; i > 0; i--) {
+void masukanElemenKeAwal(string a, string b, float c, int d, string e)
+{
+    if (currentSize < MAX_SIZE)
+    {
+        for (int i = currentSize; i > 0; i--)
+        {
             nama[i] = nama[i - 1];
             warna[i] = warna[i - 1];
             berat[i] = berat[i - 1];
@@ -87,8 +69,10 @@ void masukanElemenKeAwal(string a, string b, float c, int d, string e) {
     }
 }
 
-void masukanElemenKeAkhir(string a, string b, float c, int d, string e) {
-    if (currentSize < MAX_SIZE) {
+void masukanElemenKeAkhir(string a, string b, float c, int d, string e)
+{
+    if (currentSize < MAX_SIZE)
+    {
         nama[currentSize] = a;
         warna[currentSize] = b;
         berat[currentSize] = c;
@@ -98,8 +82,10 @@ void masukanElemenKeAkhir(string a, string b, float c, int d, string e) {
     }
 }
 
-void rubahDataElemenPertama(string a, string b, float c, int d, string e) {
-    if (currentSize > 0) {
+void rubahDataElemenPertama(string a, string b, float c, int d, string e)
+{
+    if (currentSize > 0)
+    {
         nama[0] = a;
         warna[0] = b;
         berat[0] = c;
@@ -108,8 +94,10 @@ void rubahDataElemenPertama(string a, string b, float c, int d, string e) {
     }
 }
 
-void rubahDataElemenTerakhir(string a, string b, float c, int d, string e) {
-    if (currentSize > 0) {
+void rubahDataElemenTerakhir(string a, string b, float c, int d, string e)
+{
+    if (currentSize > 0)
+    {
         int lastIndex = currentSize - 1;
         nama[lastIndex] = a;
         warna[lastIndex] = b;
@@ -119,9 +107,12 @@ void rubahDataElemenTerakhir(string a, string b, float c, int d, string e) {
     }
 }
 
-void hapusElemenPertama() {
-    if (currentSize > 0) {
-        for (int i = 0; i < currentSize - 1; i++) {
+void hapusElemenPertama()
+{
+    if (currentSize > 0)
+    {
+        for (int i = 0; i < currentSize - 1; i++)
+        {
             nama[i] = nama[i + 1];
             warna[i] = warna[i + 1];
             berat[i] = berat[i + 1];
@@ -132,15 +123,100 @@ void hapusElemenPertama() {
     }
 }
 
-void hapusElemenTerakhir() {
-    if (currentSize > 0) {
+void hapusElemenTerakhir()
+{
+    if (currentSize > 0)
+    {
         currentSize--;
     }
 }
 
-void cetakSingleArray() {
-    for (int i = 0; i < currentSize; i++) {
+void cetakSingleArray()
+{
+    for (int i = 0; i < currentSize; i++)
+    {
         cout << nama[i] << " >> ";
     }
     cout << "!!\n";
+}
+
+void cetakSeluruhAtributElemen()
+{
+    for (int i = 0; i < currentSize; i++)
+    {
+        cout << "Elemen ke-" << i + 1 << ":\n";
+        cout << "Nama: " << nama[i] << endl;
+        cout << "Warna: " << warna[i] << endl;
+        cout << "Berat: " << berat[i] << endl;
+        cout << "Harga: " << harga[i] << endl;
+        cout << "Rasa: " << rasa[i] << endl
+             << endl;
+    }
+}
+
+void cetakArray()
+{
+    cout << endl
+         << "01. Membuat Elemen Pertama:" << endl;
+    buatElemen("Mangga", "Jingga", 1.1, 10000, "Manis");
+    cetakSingleArray();
+
+    cout << endl
+         << "02. Menambahkan elemen di awal:" << endl;
+    masukanElemenKeAwal("Pisang", "Kuning", 1.3, 8000, "Manis");
+    cetakSingleArray();
+
+    cout << endl
+         << "03. Menambahkan elemen di akhir:" << endl;
+    masukanElemenKeAkhir("Apel", "Merah", 2.1, 13000, "Manis");
+    cetakSingleArray();
+
+    cout << endl
+         << "04. Menambahkan elemen di awal lagi:" << endl;
+    masukanElemenKeAwal("Anggur", "Ungu", 1, 9000, "Manis");
+    cetakSingleArray();
+
+    cout << endl
+         << "05. Menghapus elemen di awal:" << endl;
+    hapusElemenPertama();
+    cetakSingleArray();
+
+    cout << endl
+         << "06. Menghapus elemen di akhir:" << endl;
+    hapusElemenTerakhir();
+    cetakSingleArray();
+
+    cout << endl
+         << "07. Menghapus elemen di akhir lagi:" << endl;
+    hapusElemenTerakhir();
+    cetakSingleArray();
+
+    cout << endl
+         << "08. Menambahkan elemen pada posisi awal:" << endl;
+    masukanElemenKeAwal("Apel", "Merah", 2.1, 13000, "Manis");
+    cetakSingleArray();
+
+    cout << endl
+         << "09. Merubah elemen pada posisi awal:" << endl;
+    rubahDataElemenPertama("Anggur", "Ungu", 1, 9000, "Manis");
+    cetakSingleArray();
+
+    cout << endl
+         << "10. Merubah elemen pada posisi akhir:" << endl;
+    rubahDataElemenTerakhir("Mangga", "Jingga", 1.1, 10000, "Manis");
+    cetakSingleArray();
+
+    cout << endl
+         << "11. Mencetak seluruh atribut pada elemen:" << endl;
+    cetakSeluruhAtributElemen();
+}
+
+void waktuArray()
+{
+    auto start_time_B = chrono::high_resolution_clock::now();
+    cetakArray();
+    auto end_time_B = chrono::high_resolution_clock::now();
+    auto duration_B = chrono::duration_cast<chrono::microseconds>(end_time_B - start_time_B);
+
+    cout << "\nWaktu eksekusi Array: " << duration_B.count() << " ms" << endl;
 }
